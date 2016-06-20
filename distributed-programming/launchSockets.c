a1a27157770c87771e6d9e82a7f653d00f2aa4e7
Mihai Turcu <mihaiturcu@Mihais-MacBook-Pro.local>
2016-06-20 12:55:20 +0300

    setup pflink algorithm, launcher instance for nodes

---
+++ distributed-programming/launchSockets.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  launchSockets.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/20/16 12:42:39
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "pflink.h"
+int main(){
+	// this will launch a socket and bind it to port 5050
+	bindSocketinNewProc(5050);
+}
+
---
+++ distributed-programming/pflink.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/20/16 12:41:08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef PFLINK_H_INCLUDED
+#define PFLINK_H_INCLUDED
+int bindSocketinNewProc(int port);
+#endif
